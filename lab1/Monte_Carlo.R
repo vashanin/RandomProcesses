5d5359bb2e4fd636512d3d776fba1624c1c133ae
Vashanin <vashanin7@gmail.com>
2017-11-25 21:51:39 +0200

    Add lab2

--- lab1/Monte_Carlo.R
+++ lab1/Monte_Carlo.R
@@ -1,9 +1,5 @@
 # function that count integral using Monte Carlo method
 count_integral <- function(amount_of_simulations, lower_bound, upper_bound, f) {
-    if (upper_bound == Inf) {
-        upper_bound <- 10^10
-    }
-    
     X <- runif(amount_of_simulations, lower_bound, upper_bound)
     Y <- f(X) / (1 / (upper_bound - lower_bound))
     integral <- sum(Y) / amount_of_simulations
@@ -28,8 +24,28 @@ integral = count_integral(amount_of_simulations = amount_of_simulations,
 print(paste("Second integral value: ", integral))
 
 third_initial_function <- function(x) { 1 / (sqrt(x) * (x + 1) * (x + 1)) }
+
+# Зробимо заміну, щоб позбутися від нескінченності в межах інтегрування
+improved_third_initial_function <- function(t) {third_initial_function(1/t- 1) * 1/(t*t)}
 integral = count_integral(amount_of_simulations = amount_of_simulations,
                           lower_bound = 0,
-                          upper_bound = Inf,
-                          f = third_initial_function)
+                          upper_bound = 1,
+                          f = improved_third_initial_function)
 print(paste("Third integral value: ", integral))
+
+X <- runif(amount_of_simulations, 0, 2)
+Y <- runif(amount_of_simulations, 0, 2)
+
+general_amount = length(X) * length(Y)
+fitted_amount <- 0
+
+for (i in 1:length(X))
+    for (j in 1:length(Y)) {
+        if (Y[j] <= 1 - X[i]*X[i])
+            if ((X[i] - 1)^2 + (Y[j] - 1)^2 <= 1)      
+                if (Y[j] >= (X[i] - 1)^2)
+                    fitted_amount <- fitted_amount + 1
+        
+    }
+square <- 4 * fitted_amount / general_amount
+print(paste("Area: ", square))
---
+++ lab3/task3.R
@@ -0,0 +1,15 @@
+k = 5
+n = 17
+
+lambda <- 5
+
+P <- function(n, t) {((lambda * t)^n) * exp(-lambda * t) / factorial(n)}
+
+result <- c()
+previous <- 0
+
+for (t in 1:20) {
+    current <- P(n, t)
+    result <- append(result, current)
+}
+hist(P(n))
