5ab26f340a5b4816925cc43de3058c6c7517f57f
Vashanin <vashanin7@gmail.com>
2017-11-28 03:29:55 +0200

    Add part of fourth lab & Minor changes in other

--- lab3/task2.R
+++
@@ -1,7 +0,0 @@
-amount_of_simulations = as.integer(readline(prompt = "Enter amount of simulations: "))
-lambda = as.integer(readline(prompt = "Enter lambda coefficient of Poisson distribution: "))
-
-dataset <- rpois(amount_of_simulations, lambda)
-
-hist(dataset)
-plot(dataset)
